4dfe2bb80b75706a18026f42d7601fb1edab31d2
Paulo Henrique <paulinho.pawer14@gmail.com>
2024-05-07 23:07:27 -0300

    Atualização do site do jogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
-const perguntas = document.querySelectorAll(".perguntas button")
+// Script do menu mobile
 
-function ativarPergunta(event){
-    const pergunta = event.currentTarget
-    const controls = pergunta.getAttribute("aria-controls")
-    const resposta = document.getElementById(controls)
+const menuMobile = document.querySelector(".menu-mobile")
 
-    resposta.classList.toggle("ativa")
-    const ativa = resposta.classList.contains("ativa")
-    pergunta.setAttribute("aria-expanded", ativa)
+// Script da seção dúvidas do jogo
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
-function eventosPerguntas(pergunta){
-    pergunta.addEventListener("click", ativarPergunta)
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
-perguntas.forEach(eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
 
-
-new SimpleAnime()
+new SimpleAnime();
