9fa1fd4da8c8f4d0aeeaf3bc338db43c9ff84987
Paulo Henrique <paulinho.pawer14@gmail.com>
2024-05-09 21:35:23 -0300

    Adicionando o media queries 1/6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // Script do menu mobile
 
-const menuMobile = document.querySelector(".menu-mobile")
+const menuMobile = document.querySelector(".header-menu")
+
+function btnMenuMobile(){
+  if(menuMobile.style.display === "block"){
+    menuMobile.style.display = "none"
+  }else{
+    menuMobile.style.display = "block"
+  }
+}
 
 // Script da seção dúvidas do jogo
 const perguntas = document.querySelectorAll(".perguntas button");
