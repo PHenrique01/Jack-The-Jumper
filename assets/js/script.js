c63bbe6427a702841f97ca9345a7f8e35a4b219f
Paulo Henrique <paulinho.pawer14@gmail.com>
2024-06-25 22:50:26 -0300

    Adicionando o modal no botão baixar jogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,72 @@
 // Script do menu mobile
 
-const menuMobile = document.querySelector(".header-menuMobile");
+function menuMobile() {
+  const menuMobile = document.querySelector(".header-menuMobile");
 
-function ativarMenuMobile(event) {
-  if (event.type === "touchstart") event.preventDefault();
-  const nav = document.querySelector(".header-list");
-  nav.classList.toggle("ativar");
+  function ativarMenuMobile(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    const nav = document.querySelector(".header-list");
+    nav.classList.toggle("ativar");
 
-  const active = nav.classList.contains("ativar");
-  event.currentTarget.setAttribute("aria-expanded", active);
+    const active = nav.classList.contains("ativar");
+    event.currentTarget.setAttribute("aria-expanded", active);
 
-  if (active) {
-    event.currentTarget.setAttribute("aria-label", "Fechar");
-  } else {
-    event.currentTarget.setAttribute("aria-label", "Abrir");
+    if (active) {
+      event.currentTarget.setAttribute("aria-label", "Fechar");
+    } else {
+      event.currentTarget.setAttribute("aria-label", "Abrir");
+    }
   }
+
+  menuMobile.addEventListener("click", ativarMenuMobile);
+  menuMobile.addEventListener("touchstart", ativarMenuMobile);
 }
 
-menuMobile.addEventListener("click", ativarMenuMobile);
-menuMobile.addEventListener("touchstart", ativarMenuMobile);
+menuMobile();
 
 // Script da seção dúvidas do jogo
-const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta(event) {
-  const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute("aria-controls");
-  const resposta = document.getElementById(controls);
+function perguntasJogo() {
+  const perguntas = document.querySelectorAll(".perguntas button");
 
-  resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa");
-  pergunta.setAttribute("aria-expanded", ativa);
+  function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+  }
+
+  function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+  }
+
+  perguntas.forEach(eventosPerguntas);
 }
 
-function eventosPerguntas(pergunta) {
-  pergunta.addEventListener("click", ativarPergunta);
+perguntasJogo();
+
+// Script para ativar o modal
+
+function ativarModal() {
+  const jogoModal = document.querySelector("#jogo-modal");
+  const btnModal = document.querySelector(".btn-modal");
+  const btnCloseModal = document.querySelector(".btn-close");
+
+  function abrirModal() {
+    jogoModal.classList.add("ativo");
+  }
+
+  function fecharModal() {
+    jogoModal.classList.remove("ativo");
+  }
+
+  btnModal.addEventListener("click", abrirModal);
+  btnCloseModal.addEventListener("click", fecharModal);
 }
 
-perguntas.forEach(eventosPerguntas);
+ativarModal();
 
 new SimpleAnime();
